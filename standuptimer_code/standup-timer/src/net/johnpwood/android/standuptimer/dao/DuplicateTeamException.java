00b9a5cecc33a0961261e04130a553855f80abec
khaptonstall <kchaptonstall@hotmail.com>
2014-06-20 13:55:53 -0500

    Consolidated Files/Folders

--- standuptimer_code/standup-timer/src/net/johnpwood/android/standuptimer/dao/DuplicateTeamException.java
+++
@@ -1,9 +0,0 @@
-package net.johnpwood.android.standuptimer.dao;
-
-public class DuplicateTeamException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-
-    public DuplicateTeamException(String message) {
-        super(message);
-    }
-}
