f10a1524d5db6edd098ef8487019aec897ead69e
msingletary <meredithsingletary@gmail.com>
2014-07-10 13:30:36 -0500

    deleted old app source code

--- StandUpTimer/standuptimer_code/standup-timer/src/net/johnpwood/android/standuptimer/dao/InvalidTeamNameException.java
+++
@@ -1,9 +0,0 @@
-package net.johnpwood.android.standuptimer.dao;
-
-public class InvalidTeamNameException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-
-    public InvalidTeamNameException(String message) {
-        super(message);
-    }
-}
--- TomDroid/results_tomdroid_src_random/results_src_random/coverage/3/Copertura.bat
+++
@@ -1,3 +0,0 @@
-set EMMA=java -cp %EMULATORPATH%\lib\emma_device.jar emma
-%EMMA% merge -in coverage.em,coverage_3_3.ec -out coverage0.es
-%EMMA% report -r html -sp %APKPATH%\src -Dreport.sort=+name -in coverage0.es
