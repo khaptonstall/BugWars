23757d78800ef5e02a0762c2724669dbdbd93354
msingletary <meredithsingletary@gmail.com>
2014-07-22 13:09:35 -0500

    Parsers v1.3.1

--- Parsers/main.cpp
+++ Parsers/main.cpp
@@ -20,110 +20,87 @@
  *    If running JUnitParser & XMLParser at the same time, it will automatically parse these same files into XML.
  */
 
-//#include "main.h"
-
-
 #include <iostream>
 #include <string>
 
-#include "HexMatch.h"
-#include "JUnitParser.h" //declaration for jparse(bool multiple)#include "XMLParser.h" //declaration for xmlparse
+#include "JUnitParser.h" //declaration for jparse(bool multiple)#include "XMLParser.h" //declaration for xmlparse
+#include "HexMatch.h"
 using namespace std;
 
 bool readKey(char selection);
+int main();
 
 int main() {
+
 	string jListName;
 	char selection;
 	bool parseJUnit, parseMultipleJUnit, parseXML;
-	bool hexing = false;
-	bool parsing = false;
-	bool simpleParse = false;
 	cout << "-----------------" << endl;
- 	parseXML = true;
 
+	cout << "Run the JUnit parser (y/n): " << endl;
+	cin >> selection;
+	if (readKey(selection)) {
+		parseJUnit = true;
+		cout << "Parse multiple test suites (y/n): " << endl;
+		cin >> selection;
+		if (readKey(selection)) {
+			cout << "Name of the file containing the path & list of test suites: " << endl;
+			cin >> jListName;
+			setJListFile(jListName);
+			parseMultipleJUnit = true;
+		} else {
+			cout << "Path of the test suite: " << endl;
+			string jpath;
+			cin >> jpath;
+			setPath(jpath);
+			parseMultipleJUnit = false;
+		}
+	} else {
+		parseJUnit = false;
+	}
+
+	cout << "Run the XML parser (y/n): " << endl;
+	cin >> selection;
+	if (readKey(selection)) {
+		//if also running JUnitParser,
+		//Runs XMLParser on the same files JUnitParser just finished parsing
+		if (parseJUnit) {
+			string xListName = setJListFile(jListName);
+			setXFileName(xListName);
+		} else {
+			cout << "Enter the name of the file containing the path & list of test suites to add to the xml file:"  << endl;
+			string xListName;
+			cin >> xListName;
+			setXFileName(xListName);
+		}
+		if (!isRPathSet()) {
+			cout << "Enter the full path of the R.java file for the android application being tested:" << endl;
+			string rpath;
+			cin >> rpath;
+			setRPath(rpath);
+		}
+		parseXML = true;
+	} else {
+		parseXML = false;
+	}
+
+	//runs the JUnitParser.cpp
+	if (parseJUnit) {
+		if (parseMultipleJUnit) {
+			jparse(parseMultipleJUnit);
+		}
+		else {
+			jparse(false);
+		}
+	}
+
+	//runs XMLParser.cpp
 	if (parseXML) {
-		setXFileName("fileList2_p.txt");
-		setRPath("C:/Users/Meredith/AppData/Local/Android/APKs/WordPress-Source/WordPress-Android-2.0/gen/org/wordpress/android/R.java");
 		xmlparse();
 	}
 
-	if (hexing) {
-		cout << "1690.. " << intToHex(16908298) << endl;
-		cout << "2131" << intToHex(2131165257) << endl;
-		//cout << findWidgetName(16908298) << endl;
-	}
+	cout << "DONE." << endl;
 
-	if (simpleParse) {
-		setPath("WP_2_4.java");
-		jparse(false);
-	}
-
-	if (parsing) {
-		cout << "Do you want to run the JUnit parser? y/n: " << endl;
-		cin >> selection;
-		if (readKey(selection)) {
-			parseJUnit = true;
-			cout << "Do you want to parse multiple test suites? y/n: " << endl;
-			cin >> selection;
-			if (readKey(selection)) {
-				cout << "What is the name of the file containing the path & list of test suites? " << endl;
-				cin >> jListName;
-				setJListFile(jListName);
-				parseMultipleJUnit = true;
-			} else {
-				cout << "What is the path of the test suite? " << endl;
-				string jpath;
-				cin >> jpath;
-				setPath(jpath);
-				parseMultipleJUnit = false;
-			}
-		} else {
-			parseJUnit = false;
-		}
-
-		cout << "Do you want to run the XML parser? y/n: " << endl;
-		cin >> selection;
-		if (readKey(selection)) {
-			//if also running JUnitParser,
-			//Runs XMLParser on the same files JUnitParser just finished parsing
-			if (parseJUnit) {
-				string xListName = setJListFile(jListName);
-				setXFileName(xListName);
-			} else {
-				cout << "Enter the name of the file containing the path & list of test suites to add to the xml file:"  << endl;
-				string xListName;
-				cin >> xListName;
-				setXFileName(xListName);
-			}
-			if (!isRPathSet()) {
-				cout << "Enter the full path of the R.java file for the android application being tested:" << endl;
-				string rpath;
-				cin >> rpath;
-				setRPath(rpath);
-			}
-			parseXML = true;
-		} else {
-			parseXML = false;
-		}
-
-		//runs the JUnitParser.cpp
-		if (parseJUnit) {
-			if (parseMultipleJUnit) {
-				jparse(parseMultipleJUnit);
-			}
-			else {
-				jparse(false);
-			}
-		}
-
-		//runs XMLParser.cpp
-		if (parseXML) {
-			xmlparse();
-		}
-
-		cout << "DONE." << endl;
-	}
 	return 0;
 }
 
